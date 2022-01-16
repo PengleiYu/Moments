d95d9bd46429b4b18f22d107274c54efadede73c
yupenglei <yupengniaosanshi@gmail.com>
2022-01-16 23:19:13 +0800

    创建itemUI

--- app/src/main/java/com/utopia/moments/MainActivity.kt
+++ app/src/main/java/com/utopia/moments/MainActivity.kt
@@ -3,10 +3,15 @@ package com.utopia.moments
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.utopia.moments.ui.theme.MomentsTheme
 
@@ -17,22 +22,49 @@ class MainActivity : ComponentActivity() {
       MomentsTheme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
-          Greeting("Android")
+          val data = ItemData(50f, "Title", "caption", 50f, "%")
+          Item(data = data)
         }
       }
     }
   }
 }
 
+data class ItemData(
+  val progress: Float,
+  val title: String,
+  val caption: String,
+  val number: Float,
+  val unit: String
+)
+
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun Item(data: ItemData) {
+  Row {
+    CircularProgressIndicator(
+      progress = data.progress / 100f,
+    )
+    Column(
+      Modifier.weight(1f),
+    ) {
+      Text(text = data.title)
+      Text(text = data.caption)
+    }
+    Column(horizontalAlignment = Alignment.End) {
+      Text(text = data.number.toString())
+      Text(text = data.unit)
+    }
+  }
 }
 
-@Preview(showBackground = true)
+@Preview(
+  showBackground = true,
+  showSystemUi = true,
+)
 @Composable
 fun DefaultPreview() {
   MomentsTheme {
-    Greeting("Android")
+    val data = ItemData(50f, "Title", "caption", 50f, "%")
+    Item(data = data)
   }
 }
