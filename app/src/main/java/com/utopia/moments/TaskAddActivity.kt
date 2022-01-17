812f505b303760d828aebdb3d245e6c09ba24d89
yupenglei <yupengniaosanshi@gmail.com>
2022-01-17 13:49:06 +0800

    新建页添加editText和timePicker

--- app/src/main/java/com/utopia/moments/TaskAddActivity.kt
+++ app/src/main/java/com/utopia/moments/TaskAddActivity.kt
@@ -4,7 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TaskAddActivity : AppCompatActivity() {
   private lateinit var taskType: TaskType
@@ -16,7 +27,43 @@ class TaskAddActivity : AppCompatActivity() {
     taskType = TaskType.valueOf(extraTaskType)
 
     setContent {
-      Text(text = "Hello, taskType=$taskType")
+      Scaffold(
+        topBar = {
+          TopAppBar(
+            title = {
+              Text(text = "新建")
+            },
+            navigationIcon = {
+              IconButton(onClick = { finish() }) {
+                Icon(Icons.Default.ArrowBack, "back")
+              }
+            },
+          )
+        },
+        content = {
+          Column {
+            val text = rememberSaveable { mutableStateOf("Text") }
+            TextField(
+              value = text.value,
+              onValueChange = { text.value = it },
+              label = {
+                Text(text = "命名你的进度")
+              }
+            )
+
+            Text(text = "开始时间")
+            val fromDate = remember { mutableStateOf(System.currentTimeMillis()) }
+            Button(onClick = {
+              showDatePicker(this@TaskAddActivity) {
+                val time = it ?: return@showDatePicker
+                fromDate.value = time
+              }
+            }) {
+              Text(text = convertLongToDate(fromDate.value))
+            }
+          }
+        }
+      )
     }
   }
 
@@ -30,3 +77,26 @@ class TaskAddActivity : AppCompatActivity() {
   }
 }
 
+private fun convertLongToDate(time: Long): String {
+  val date = Date(time)
+  val format = SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.CHINA)
+  return format.format(date)
+}
+
+private fun showDatePicker(
+  activity: AppCompatActivity,
+  updateDate: (Long?) -> Unit
+) {
+  val picker = MaterialDatePicker.Builder.datePicker().build()
+  picker.show(activity.supportFragmentManager, picker.toString())
+  picker.addOnPositiveButtonClickListener {
+    updateDate(it)
+  }
+}
+
+@Preview(
+  showBackground = true
+)
+@Composable
+private fun PreviewTaskAdd() {
+}
